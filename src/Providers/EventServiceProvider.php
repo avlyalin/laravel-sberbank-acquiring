2cf77ff983cb82242fc4c8d8529fae622b5d5f23
avlyalin <lyalin.alexey@gmail.com>
2020-07-01 19:02:58 +0300

    fix: event service provider

---
+++ src/Providers/EventServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Avlyalin\SberbankAcquiring\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+
+}
