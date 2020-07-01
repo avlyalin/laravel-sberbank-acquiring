38819bf06e245a254b40a6c784f94e2126bab269
avlyalin <lyalin.alexey@gmail.com>
2020-07-01 18:03:25 +0300

    fix: sberbank payment test factory

--- database/factories/SberbankPaymentFactory.php
+++ database/factories/SberbankPaymentFactory.php
@@ -15,6 +15,15 @@ $factory->define(SberbankPayment::class, function (Faker $faker) {
         'currency' => $faker->currencyCode,
         'return_url' => $faker->url,
         'fail_url' => $faker->url,
+        'description' => $faker->sentence,
+        'language' => $faker->languageCode,
+        'client_id' => Str::random(20),
+        'page_view' => $faker->randomElement(['MOBILE', 'DESKTOP']),
+        'merchant_login' => Str::random(10),
+        'json_params' => json_encode([$faker->word => $faker->word, $faker->word => $faker->word]),
+        'session_timeout_secs' => $faker->randomNumber(9),
+        'expiration_date' => $faker->dateTimeBetween('+1 hour', '+2 hour'),
+        'features' => Str::random('10'),
         'bank_form_url' => $faker->url,
     ];
 });
