6c3ff25d828b3415066dfd2ed4eb85c6138e43f4
avlyalin <lyalin.alexey@gmail.com>
2020-07-04 20:46:07 +0300

    feat: return payment status "new"

--- src/Models/DictAcquiringPaymentStatus.php
+++ src/Models/DictAcquiringPaymentStatus.php
@@ -5,59 +5,66 @@ namespace Avlyalin\SberbankAcquiring\Models;
 class DictAcquiringPaymentStatus extends BaseModel
 {
     /**
+     * Новый
+     *
+     * @var string
+     */
+    public const NEW = 1;
+
+    /**
      * Зарегистрирован
      *
      * @var string
      */
-    public const REGISTERED = 1;
+    public const REGISTERED = 2;
 
     /**
      * Захолдирован
      *
      * @var string
      */
-    public const HELD = 2;
+    public const HELD = 3;
 
     /**
      * Подтвержден
      *
      * @var string
      */
-    public const CONFIRMED = 3;
+    public const CONFIRMED = 4;
 
     /**
      * Отменен
      *
      * @var string
      */
-    public const REVERSED = 4;
+    public const REVERSED = 5;
 
     /**
      * Оформлен возврат
      *
      * @var string
      */
-    public const REFUNDED = 5;
+    public const REFUNDED = 6;
 
     /**
      * ACS-авторизация
      * @var string
      */
-    public const ACS_AUTH = 6;
+    public const ACS_AUTH = 7;
 
     /**
      * Ошибка
      *
      * @var string
      */
-    public const AUTH_DECLINED = 7;
+    public const AUTH_DECLINED = 8;
 
     /**
      * Системная ошибка при обработке платежа
      *
      * @var string
      */
-    public const ERROR = 8;
+    public const ERROR = 9;
 
     protected $tableNameKey = 'dict_payment_statuses';
 }
